8a01e04b60abef16067474e5d87e7bfd2662cf66
vagrant <nabilahmed98@hotmail.com>
2022-09-02 22:17:09 +0000

    first c

---
+++ breedFetcher.js
@@ -0,0 +1,28 @@
+// const request = require("request");
+
+// const fetchBreedDescription = function(breedName, callback) {
+//   let URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+
+//   request(URL, (error, response, body) => {
+//     console.log('statusCode:', response && response.statusCode));
+//   }
+// }
+const request = require("request");
+
+const fetchBreedDescription = function (breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, resp, body) => {
+    if (error) {
+      callback(`Failed to request details: ${error}`, null);
+    }
+
+    const data = JSON.parse(body);
+
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback(`Failed to find breed ${breedName}`, null);
+    }
+  });
+};
